c7281227af5ae120e5e9c5daa7587a949bafa44c
notobject <517609390@qq.com>
2017-07-22 09:07:01 +0800

    修改

--- src/com/ddjc/basic/JavaHttpRequest.java
+++ src/com/ddjc/basic/JavaHttpRequest.java
@@ -1,17 +1,14 @@
 package com.ddjc.basic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class JavaHttpRequest {
 	@Autowired
@@ -74,68 +71,68 @@ public class JavaHttpRequest {
         return result;
     }
 
-    /**
-     * 向指定 URL 发送POST方法的请求
-     * 
-     * @param url
-     *            发送请求的 URL
-     * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendPost(String url, String param) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try {
-            URL realUrl = new URL(url);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setRequestProperty("Accept-Charset", "utf-8");
-            conn.setRequestProperty("contentType", "utf-8");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-  /*          in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));*/
-            in = new BufferedReader(new InputStreamReader(
-            		conn.getInputStream(),"utf-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
-            e.printStackTrace();
-        }
-        //使用finally块来关闭输出流、输入流
-        finally{
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(in!=null){
-                    in.close();
-                }
-            }
-            catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
-        return result;
-    }    
+//    /**
+//     * 向指定 URL 发送POST方法的请求
+//     *
+//     * @param url
+//     *            发送请求的 URL
+//     * @param param
+//     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+//     * @return 所代表远程资源的响应结果
+//     */
+//    public static String sendPost(String url, String param) {
+//        PrintWriter out = null;
+//        BufferedReader in = null;
+//        String result = "";
+//        try {
+//            URL realUrl = new URL(url);
+//            // 打开和URL之间的连接
+//            URLConnection conn = realUrl.openConnection();
+//            // 设置通用的请求属性
+//            conn.setRequestProperty("accept", "*/*");
+//            conn.setRequestProperty("connection", "Keep-Alive");
+//            conn.setRequestProperty("user-agent",
+//                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+//            conn.setRequestProperty("Accept-Charset", "utf-8");
+//            conn.setRequestProperty("contentType", "utf-8");
+//            // 发送POST请求必须设置如下两行
+//            conn.setDoOutput(true);
+//            conn.setDoInput(true);
+//            // 获取URLConnection对象对应的输出流
+//            out = new PrintWriter(conn.getOutputStream());
+//            // 发送请求参数
+//            out.print(param);
+//            // flush输出流的缓冲
+//            out.flush();
+//            // 定义BufferedReader输入流来读取URL的响应
+//  /*          in = new BufferedReader(
+//                    new InputStreamReader(conn.getInputStream()));*/
+//            in = new BufferedReader(new InputStreamReader(
+//            		conn.getInputStream(),"utf-8"));
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                result += line;
+//            }
+//        } catch (Exception e) {
+//            System.out.println("发送 POST 请求出现异常！"+e);
+//            e.printStackTrace();
+//        }
+//        //使用finally块来关闭输出流、输入流
+//        finally{
+//            try{
+//                if(out!=null){
+//                    out.close();
+//                }
+//                if(in!=null){
+//                    in.close();
+//                }
+//            }
+//            catch(IOException ex){
+//                ex.printStackTrace();
+//            }
+//        }
+//        return result;
+//    }
 
     public static void main(String[] args) {
         //发送 GET 请求
@@ -143,9 +140,9 @@ public class JavaHttpRequest {
     	//HttpServletRequest request=ServletActionContext.getRequest();;
        // String s=HttpRequest.sendGet("http://localhost:6144/Home/RequestString", "key=123&v=456");
     	//String s=((JavaHttpRequest) request1).sendGet("http://localhost:8080//ddjc/user/login", "data=111");
-    	JavaHttpRequest java=new JavaHttpRequest();
-    	String s=java.sendGet("http://localhost:8080//ddjc/user", "op=login&data=111");
-    	System.out.println(s);
+//    	JavaHttpRequest java=new JavaHttpRequest();
+//    	String s=java.sendGet("http://localhost:8080//ddjc/user", "op=login&data=111");
+//    	System.out.println(s);
         
         //发送 POST 请求HttpServletRequest
  //       String sr=((JavaHttpRequest) request1).sendPost("http://localhost:6144/Home/RequestPostString", "key=123&v=456");
--- src/com/ddjc/controller/CareloveController.java
+++ src/com/ddjc/controller/CareloveController.java
@@ -1,107 +1,105 @@
 package com.ddjc.controller;
 
-import java.util.List;
-
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.ddjc.entity.User;
+import com.ddjc.entity.Userfriend;
+import com.ddjc.entity.appmodel.FriendModel;
+import com.ddjc.json.AppJsonMessage;
+import com.ddjc.service.NurseUserService;
+import com.ddjc.service.UserService;
+import com.ddjc.service.UserfriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.ddjc.entity.Nurseuser;
-import com.ddjc.entity.User;
-import com.ddjc.entity.Userfriend;
-import com.ddjc.entity.appmodel.FriendModel;
-import com.ddjc.entity.appmodel.registModel;
-import com.ddjc.json.AppJsonMessage;
-import com.ddjc.service.NurseUserService;
-import com.ddjc.service.UserService;
-import com.ddjc.service.UserfriendService;
-import com.ddjc.utils.UuidUtil;
+import java.util.List;
 
 @Controller
 @RequestMapping("/carelove")
 public class CareloveController {
-	@Autowired
-	private NurseUserService nurseuserservice;
-	@Autowired
-	private UserService userservice;
-	@Autowired
-	private UserfriendService userfriendservice;
-	    @ResponseBody
-	    @RequestMapping(value = "/addFamily")
-		public AppJsonMessage addFamily(String data){ 	
-	    	AppJsonMessage app=new AppJsonMessage();
-	    	JSONObject jsonObj = JSON.parseObject(data);
-	    	String id=jsonObj.getString("id");
-	    	String userType=jsonObj.getString("userType");
-	    	String firendcode=jsonObj.getString("firendcode");
-	    	String relationMark=jsonObj.getString("relationMark");
-	    	
-	    	User user=userservice.findUserBycode(firendcode);
-	    	if(user==null){
-	    		app.setCode(4021);
-	        	app.setMsg("该亲友不存在");
-	        	app.setData("");
-	        	return app;
-	    	}
-	    	if(user.getId().equals(id)){
-	    		app.setCode(4023);
-	        	app.setMsg("添加失败，不能添加自己");
-	        	app.setData("");
-	        	return app;
-	    	}
-	    	if(userType.equals("1")){
-	    		int unrse=nurseuserservice.findByIdAndUsercode(id,firendcode,relationMark);
-	    		if (unrse==2){
-	    			app.setCode(4024);
-		        	app.setMsg("您已添加该好友，无须重复添加");
-		        	app.setData("");
-		        	return app;
-	    		}else{
-	    			app.setCode(0);
-		        	app.setMsg("添加成功");
-		        	app.setData("");
-		        	return app;
-	    		}
-	    	}else{
-	    		Userfriend userfriend=userfriendservice.findByIds(id,user.getId());
-	    		if(userfriend!=null){
-	    			app.setCode(4024);
-		        	app.setMsg("您已添加该好友，无须重复添加");
-		        	app.setData("");
-	    		}else{
-	    			
-	    			int mark=userfriendservice.insertFriend(id,user,relationMark);	    			
-	    			if(mark>1){
-	    				app.setCode(0);
-			        	app.setMsg("添加成功");
-			        	app.setData("");
-	    			}
-	    			
-	    		}
-	    		return app;
-	    	}
 
-		}
-	    
-	@ResponseBody
+    @Autowired
+    private NurseUserService nurseuserservice;
+    @Autowired
+    private UserService userservice;
+    @Autowired
+    private UserfriendService userfriendservice;
+
+    @ResponseBody
+    @RequestMapping(value = "/addFamily")
+    public AppJsonMessage addFamily(String data) {
+        AppJsonMessage app = new AppJsonMessage();
+        JSONObject jsonObj = JSON.parseObject(data);
+        String id = jsonObj.getString("id");
+        String userType = jsonObj.getString("userType");
+        String firendcode = jsonObj.getString("firendcode");
+        String relationMark = jsonObj.getString("relationMark");
+
+        User user = userservice.findUserBycode(firendcode);
+        if (user == null) {
+            app.setCode(4021);
+            app.setMsg("该亲友不存在");
+            app.setData("");
+            return app;
+        }
+        if (user.getId().equals(id)) {
+            app.setCode(4023);
+            app.setMsg("添加失败，不能添加自己");
+            app.setData("");
+            return app;
+        }
+        if (userType.equals("1")) {
+            int unrse = nurseuserservice.findByIdAndUsercode(id, firendcode, relationMark);
+            if (unrse == 2) {
+                app.setCode(4024);
+                app.setMsg("您已添加该好友，无须重复添加");
+                app.setData("");
+                return app;
+            } else {
+                app.setCode(0);
+                app.setMsg("添加成功");
+                app.setData("");
+                return app;
+            }
+        } else {
+            Userfriend userfriend = userfriendservice.findByIds(id, user.getId());
+            if (userfriend != null) {
+                app.setCode(4024);
+                app.setMsg("您已添加该好友，无须重复添加");
+                app.setData("");
+            } else {
+
+                int mark = userfriendservice.insertFriend(id, user, relationMark);
+                if (mark > 1) {
+                    app.setCode(0);
+                    app.setMsg("添加成功");
+                    app.setData("");
+                }
+
+            }
+            return app;
+        }
+
+    }
+
+    @ResponseBody
     @RequestMapping(value = "/getMyFamilyList")
-	public AppJsonMessage getMyFamilyList(String data){ 	
-	    AppJsonMessage app=new AppJsonMessage();
-	    JSONObject jsonObj = JSON.parseObject(data);
-	    String id=jsonObj.getString("id");
-	    String userType=jsonObj.getString("userType");
-	    List<FriendModel> friendList=null;
-	    if(userType.equals("1")){
-	    	friendList=nurseuserservice.getMyFamilyList(id);
-	    }else{
-	    	friendList=userfriendservice.getMyFamilyList(id);
-	    }
-	    app.setCode(0);
-    	app.setMsg("操作成功");
-    	app.setData(friendList);
-	    return app;
-	}
+    public AppJsonMessage getMyFamilyList(String data) {
+        AppJsonMessage app = new AppJsonMessage();
+        JSONObject jsonObj = JSON.parseObject(data);
+        String id = jsonObj.getString("id");
+        String userType = jsonObj.getString("userType");
+        List<FriendModel> friendList = null;
+        if (userType.equals("1")) {
+            friendList = nurseuserservice.getMyFamilyList(id);
+        } else {
+            friendList = userfriendservice.getMyFamilyList(id);
+        }
+        app.setCode(0);
+        app.setMsg("操作成功");
+        app.setData(friendList);
+        return app;
+    }
 }
